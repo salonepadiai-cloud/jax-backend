a5e5bdeedb2ac590413d94692c8ea9e2c63c6795
salonepadiai-cloud <salonepadiai@gmail.com>
2026-09-11 10:04:29 +0000

    Delete Iroutes directory

--- Iroutes/status.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getStatus } from "../controllers/statusController.js";
-
-const router = express.Router();
-
-router.get("/", getStatus);
-
-export default router;
